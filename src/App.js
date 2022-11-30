cdc46acc11a04ff18f788d89e5f84d5a48783bf5
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-11-30 18:06:07 -0400

    Change funtinal component to a class on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from 'react';
 import './App.css';
 import MyCalculator from './components/Calculator';
 
-function App() {
-  return (
-    <div>
-      <MyCalculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <MyCalculator />
+      </div>
+    );
+  }
 }
 
 export default App;
