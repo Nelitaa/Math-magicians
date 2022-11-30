086ad6e44c17256de5d262a404f159cbacd62bcc
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-11-30 17:20:34 -0400

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyCalculator from './components/Calculator';
+
+function App() {
+  return (
+    <div>
+      <MyCalculator />
+    </div>
+  );
+}
+
+export default App;
