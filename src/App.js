205b618723c36ff20a28f2fedb107caa372c3d9e
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-01 13:27:27 -0400

    Refactor Calculator.js and App.js with hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React from 'react';
 import './App.css';
 import MyCalculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <MyCalculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <MyCalculator />
+    </div>
+  );
 }
 
 export default App;
