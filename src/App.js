dafc1707491786896f5b593b0ea6ac428b44e8d1
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-06 17:40:45 -0400

    Add component Home.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Link,
 } from 'react-router-dom';
 import './App.css';
 import MyCalculator from './components/Calculator';
@@ -10,13 +12,31 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/quote" element={<Quote />} />
-        <Route path="/calculator" element={<MyCalculator />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div>
+      <header>
+        <nav>
+          <h1>Math Magicians</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Calculator</Link>
+            </li>
+            <li>
+              <Link to="/quote">Quote</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Router>
+        <Routes>
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/calculator" element={<MyCalculator />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
