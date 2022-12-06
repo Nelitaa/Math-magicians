8c1d1750475d6a3d774e964615ca68adac1dcae5
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-06 16:59:14 -0400

    Setup React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 import MyCalculator from './components/Calculator';
+import Quote from './components/Quote';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      <MyCalculator />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/calculator" element={<MyCalculator />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
