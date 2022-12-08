d74b1367ae0f17135c23842650c0ba8033b1ee49
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-08 16:05:13 -0400

    Revert "Math magicians project"

--- src/tests/Quote.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import Quote from '../components/Quote';
-
-test('renders Quote component', () => {
-  const { asFragment } = render(<Quote />);
-  expect(asFragment()).toMatchSnapshot();
-});
